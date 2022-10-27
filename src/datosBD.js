906ce3316eacc81baddbe2a0b9ca36db160da1de
CENTRALSYS\vnemec <virginia.nemec@gmail.com>
2022-10-27 10:19:05 -0300

    datos

--- src/datosBD.js
+++ src/datosBD.js
@@ -2,31 +2,31 @@ export default {
   negocios: [
     {
       id: 1,
-      name: 'Poroto Moreno',
-      category: 'Food Natural',
+      name: "Poroto Moreno",
+      category: "Food Natural",
       orders: [
         {
           id: 1,
-          usuario: '',
-          fecha: '',
+          usuario: "",
+          fecha: "",
           total: 0,
           detail: [
             {
               id: 1,
-              name: 'Tostada',
+              name: "Tostada",
               price: 270,
             },
           ],
         },
         {
           id: 2,
-          usuario: '',
-          fecha: '',
+          usuario: "",
+          fecha: "",
           total: 0,
           detail: [
             {
               id: 1,
-              name: 'Gaseosa',
+              name: "Gaseosa",
               price: 180,
             },
           ],
@@ -35,38 +35,38 @@ export default {
       products: [
         {
           id: 1,
-          name: 'Sand. Vegano',
-          image: 'SandwichVegano',
+          name: "Sand. Vegano",
+          image: "SandwichVegano",
           price: 150,
           cant: 0,
         },
         {
           id: 2,
-          name: 'Ensalada variadito de primavera!',
-          image: 'ensaladaVarPrimavera',
+          name: "Ensalada variadito de primavera!",
+          image: "ensaladaVarPrimavera",
           price: 150,
           cant: 0,
         },
         {
           id: 7,
-          name: 'Arroz yamani con acelgas salteadas x 500gr',
-          image: 'arrozYamaniAcelga',
+          name: "Arroz yamani con acelgas salteadas x 500gr",
+          image: "arrozYamaniAcelga",
           price: 510,
           // no se si necesito cant.
           cant: 0,
         },
         {
           id: 8,
-          name: 'Canelones de acelga x 6',
-          image: 'CanelonesAcelga',
+          name: "Canelones de acelga x 6",
+          image: "CanelonesAcelga",
           price: 1250,
           // no se si necesito cant.
           cant: 0,
         },
         {
           id: 9,
-          name: 'Brusqueta Mediterranea x 2u',
-          image: 'brusquetaMediterranea',
+          name: "Brusqueta Mediterranea x 2u",
+          image: "brusquetaMediterranea",
           price: 1200,
           // no se si necesito cant.
           cant: 0,
@@ -75,52 +75,60 @@ export default {
       ofertas: [
         {
           id: 1,
-          name: 'Sand. Vegano',
-          image: 'SandwichVegano',
+          name: "Sand. Vegano",
+          image: "SandwichVegano",
           price: 150,
           cant: 2,
           discount: 50,
-          typeDiscount: '%',
+          typeDiscount: "%",
           // no se si necesito cant.price
-          description: 'Llevando 2, pagas el 50% cada uno!!!',
+          description: "Llevando 2, pagas el 50% cada uno!!!",
         },
       ],
     },
     {
       id: 2,
-      name: 'Sarteneando quinoa!',
-      category: 'Food Natural',
+      name: "Sarteneando quinoa!",
+      category: "Food Natural",
       orders: [],
       products: [
         {
           id: 3,
-          name: 'Ensalada de Lenteja',
-          image: 'ensaladaLenteja',
+          name: "Ensalada de Lenteja",
+          image: "ensaladaLenteja",
           price: 250,
           // no se si necesito cant.
           cant: 0,
         },
         {
           id: 4,
-          name: 'Tostadas de Frijoliltos x 1u',
-          image: 'TostadaFrijoles',
+          name: "Tostadas de Frijoliltos x 1u",
+          image: "TostadaFrijoles",
           price: 180,
           // no se si necesito cant.
           cant: 0,
         },
         {
           id: 5,
-          name: 'Tofu para la picada x 500gr',
-          image: 'picaditaTofu',
+          name: "Tofu para la picada x 500gr",
+          image: "picaditaTofu",
           price: 1600,
           // no se si necesito cant.
           cant: 0,
         },
         {
           id: 6,
-          name: 'Empanadas de soja x 12 u',
-          image: 'empanadasSojaText',
+          name: "Empanadas de soja x 12 u",
+          image: "empanadasSojaText",
           price: 1800,
+          // no se si necesito cant.
+          cant: 0,
+        },
+        {
+          id: 10,
+          name: "Empanadas de soja x 12 u",
+          image: "empanadasSojaText",
+          price: 1850,
           // no se si necesito cant.
           cant: 0,
         },
